4d3750bdb00bd5f5ab563f65816f234b1d1d28fd
maeda-ema-z <maeda-ema-z@densei.co.jp>
2020-01-10 12:52:21 +0800

    Initial Commit

---
+++ SwiftUiSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiSample
+//
+//  Created by user164140 on 1/10/20.
+//  Copyright © 2020 user164140. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
